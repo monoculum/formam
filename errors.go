40e64ba53f83ca82819ce66df836d99d9f76a617
Martin Tournoij <martin@arp242.net>
2019-06-18 16:15:18 +0200

    Add error codes
    The main reason for this is that I want to log (but not ignore) unknown
    form fields:
    
    	err = formam.Decode(r.Form, dst)
    	if fErr, ok := err.(*formam.Error); ok && fErr.Code() == formam.ErrCodeUnknownField {
    		zlog.Error(err)
    		err = nil
    	}
    	return err
    
    There was no way to get this information previously, and with
    `IgnoreUnknownKeys` I would never know if there was an unknown key.
    
    IMHO the `IgnoreUnknownKeys` option could be deprecated with this, as
    it's now redundant. But that's a backwards-incompatible change (perhaps
    something to consider for a v2?)
    
    While I was changing `newError()`,  I also changed it to pass the field
    and path and build the string with it.
    
    Note: `strings.Builder` is Go 1.10 and newer only. Previously formam
    supported a lot of quite old Go versions, and I took the liberty of
    changing that. I can change it to use string concatenation if you
    prefer, this is just a bit faster.

--- errors.go
+++ errors.go
@@ -3,18 +3,46 @@ package formam
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
+)
+
+// Error codes.
+const (
+	ErrCodeNotAPointer  uint8 = iota // Didn't pass a pointer to Decode().
+	ErrCodeArrayIndex                // Error attempting to use an array index (e.g. foo[2]).
+	ErrCodeConversion                // Error converting field to the type.
+	ErrCodeUnknownType               // Unknown type.
+	ErrCodeUnknownField              // No struct field for passed parameter (will never be used if IgnoreUnknownKeys is set).
 )
 
 type Error struct {
-	err error
+	code        uint8
+	field, path string
+	err         error
 }
 
 func (s *Error) Error() string {
-	return "formam: " + s.err.Error()
+	var b strings.Builder
+	b.WriteString("formam: ")
+	if s.field != "" {
+		b.WriteString("field=")
+		b.WriteString(s.field)
+		b.WriteString("; path=")
+		b.WriteString(s.path)
+		b.WriteString(": ")
+	}
+
+	b.WriteString(s.err.Error())
+	return b.String()
 }
 
 func (s Error) MarshalJSON() ([]byte, error) {
-	return json.Marshal(s.err.Error())
+	return json.Marshal(s.Error())
+}
+
+// Code for this error. See the ErrCode* constants.
+func (s Error) Code() uint8 {
+	return s.code
 }
 
 // Cause implements the causer interface from github.com/pkg/errors.
@@ -22,6 +50,6 @@ func (s *Error) Cause() error {
 	return s.err
 }
 
-func newError(format string, a ...interface{}) error {
-	return &Error{fmt.Errorf(format, a...)}
+func newError(code uint8, field, path, format string, a ...interface{}) error {
+	return &Error{code: code, field: field, path: path, err: fmt.Errorf(format, a...)}
 }
